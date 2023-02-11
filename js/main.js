2e2e25918d15d11613eaf30aeef73172dfcb5391
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-11 18:10:17 +0300

    Добавлена анимация

--- js/main.js
+++ js/main.js
@@ -32,4 +32,13 @@ $(function () {
       },
     ],
   });
+
+  wow = new WOW({
+    boxClass: "wow", // default
+    animateClass: "animate__animated", // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true, // default
+  });
+  wow.init();
 });
